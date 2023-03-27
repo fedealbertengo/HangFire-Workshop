7517748edbd73511ef1a60a8e4de769c896754ba
falbertengo <falbertengo@symplr.com>
2023-03-27 16:18:13 -0300

    Cleaned solution

---
+++ HangFire Workshop/HangFire Workshop/Extensions/StartupExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Hangfire.SqlServer;
+using System;
+
+namespace HangFire_Workshop.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureHangFire(this IServiceCollection services, IConfiguration Configuration)
+        {
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    DisableGlobalLocks = true
+                }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+        }
+    }
+}
--- HangFire Workshop/HangFire Workshop/Startup.cs
+++ HangFire Workshop/HangFire Workshop/Startup.cs
@@ -1,15 +1,12 @@
+using Hangfire;
 using HangFire_Workshop.Data;
+using HangFire_Workshop.Extensions;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HangFire_Workshop
 {
@@ -22,39 +19,30 @@ namespace HangFire_Workshop
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.ConfigureHangFire(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IBackgroundJobClient backgroundJobClient, IRecurringJobManager recurringJobManager)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapHangfireDashboard();
             });
+
+            app.UseHangfireDashboard();
+
+            //Tareas programadas
+            backgroundJobClient.Enqueue(() => Console.WriteLine("Holaa")); //Para ejecutar el job instantaneamente
+            backgroundJobClient.Schedule(() => Console.WriteLine("Saludos, con delay"), TimeSpan.FromSeconds(30)); //Para ejecutar el job a cierta fecha y hora
+
+            //Tareas recurrentes
+            recurringJobManager.AddOrUpdate("Tarea recurrente cada minuto", () => Console.WriteLine("Se ejecuto la tarea recurrente."), Cron.Minutely); //Para ejecutar una tarea recurrente
         }
     }
 }
